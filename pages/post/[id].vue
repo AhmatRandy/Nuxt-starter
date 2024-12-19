2cf7ae967545bd2c236520f12f2320d1ab628440
Ahmat Randy <98681609+AhmatRandy@users.noreply.github.com>
2024-12-19 15:22:59 +0700

    data fetching and routes

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
 const route = useRoute();
+const id = ref(route.params.id);
+const { data: posts } = await useAsyncData(
+  "posts",
+  () => $fetch(`https://jsonplaceholder.typicode.com/posts/${id.value}`),
+  {
+    watch: [id],
+  }
+);
+
+console.log("data", posts);
 </script>
 
 <template>
-  <div>
-    <h1>Post {{ route.params.id }}</h1>
+  <div class="max-w-2xl mx-auto p-4 bg-white shadow-md rounded-lg">
+    <h1 class="text-2xl font-bold mb-2">{{ posts.title }}</h1>
+    <p class="text-gray-700 mb-4">{{ posts.body }}</p>
+    <div class="flex justify-between">
+      <button
+        @click="() => $router.push(`/useFetch`)"
+        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        back
+      </button>
+      <button
+        class="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400"
+      >
+        Share
+      </button>
+    </div>
   </div>
 </template>
---
+++ pages/useFetch.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const router = useRouter();
+
+const { data, status, error, refresh } = await useFetch(
+  "https://jsonplaceholder.typicode.com/posts"
+);
+
+console.log("data", data);
+</script>
+
+<template>
+  <div class="p-5 bg-gray-100 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold mb-4">Daftar Post</h1>
+    <span
+      v-for="item in data"
+      :key="item.id"
+      class="block mb-2 p-3 bg-white border hover:text-blue-300 hover:underline border-gray-300 rounded hover:bg-gray-200 transition"
+      @click="() => router.push(`/post/${item.id}`)"
+    >
+      {{ item.title }}
+    </span>
+  </div>
+</template>
