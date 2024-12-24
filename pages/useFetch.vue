018d1263057007285fba405c0e04304faafb8f1b
Ahmat Randy <98681609+AhmatRandy@users.noreply.github.com>
2024-12-24 10:48:03 +0700

    learn $fetch and useFetch

--- pages/useFetch.vue
+++ pages/useFetch.vue
@@ -1,49 +1,50 @@
 <script setup lang="ts">
-const name = ref("");
+import { comment } from "postcss";
+
+const query = ref<string>("");
+const comments = ref([]);
+
 const { data } = await useFetch("https://jsonplaceholder.typicode.com/posts");
 
-async function handleFormSubmit() {
-  const res = await $fetch("/api/submit", {
-    method: "POST",
-    body: {
-      name: name.value,
-    },
-  });
+const router = useRouter();
+async function handleFormSubmit(event: any) {
+  event.preventDefault();
+  const dataTwice = await $fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query?.value}`
+  );
+
+  comments.value = dataTwice;
 }
 </script>
 
 <template>
-  <!-- <div v-if="data == null">No data</div> -->
-  <div>
+  <div class="p-5 bg-gray-100 rounded-lg shadow-md">
     <form @submit="handleFormSubmit">
-      <!-- form input tags -->
-      <label>name</label>
-      <input v-model="name" />
-      <button type="submit">submit</button>
+      <label>search comment</label>
+      <input v-model="query" />
+      <button class="bg-blue-400" type="submit">submit</button>
     </form>
+
+    <div class="flex gap-4">
+      <div class="bg-white shadow-lg rounded-md">
+        <h1>post data</h1>
+        <span
+          v-for="item in data"
+          :key="item.id"
+          class="block mb-2 p-3 bg-white border hover:text-blue-300 hover:underline border-gray-300 rounded hover:bg-gray-200 transition"
+          @click="() => router.push(`/post/${item.id}`)"
+        >
+          {{ item.title }}
+        </span>
+      </div>
+      <div class="bg-white shadow-lg rounded-md">
+        <h1>comment data</h1>
+        <!-- Menampilkan komentar jika ada -->
+        <h3>Comments:</h3>
+        <div v-for="comment in comments" :key="comment.id">
+          <p>{{ comment.body }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<!-- <script setup lang="ts">
-const router = useRouter();
-
-const postData = await $fetch("https://jsonplaceholder.typicode.com/posts");
-
-console.log("data", postData);
-</script>
-
-<template>
-  <div class="p-5 bg-gray-100 rounded-lg shadow-md">
-    <h1 class="text-2xl font-bold mb-4">Daftar Post</h1>
-    <label>name</label>
-    <input v-model="name" />
-    <span
-      v-for="item in postData"
-      :key="item.id"
-      class="block mb-2 p-3 bg-white border hover:text-blue-300 hover:underline border-gray-300 rounded hover:bg-gray-200 transition"
-      @click="() => router.push(`/post/${item.id}`)"
-    >
-      {{ item.title }}
-    </span>
-  </div>
-</template> -->
